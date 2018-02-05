46cb6b584f8e76d1140d7c4908cb6e8c32f7b921
wops <wops@PCALP149.gft.com>
2018-02-05 16:14:54 -0200

    Página de portifólio
    Alterando o método de execução do código responsável por gerar a
    animação de digitação na tela de ready para load

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-$('document').ready(function() {
+$('document').load(function() {
 	var typed = new Typed('#text-info-professional', {
 		strings: ['Wellington Gonçalves Pires' , 'Java World','Backend Developer', 'Wellington Gonçalves Pires | Java World | Backend Developer'],
 	    typeSpeed: 20,
