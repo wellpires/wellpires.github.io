47f8403e1f09b3c08f07879a44c81b2b8164f404
wops <wops@PCALP149.gft.com>
2018-02-05 16:16:01 -0200

    Voltando a correção do método anterior

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-$('document').load(function() {
+$('document').ready(function() {
 	var typed = new Typed('#text-info-professional', {
 		strings: ['Wellington Gonçalves Pires' , 'Java World','Backend Developer', 'Wellington Gonçalves Pires | Java World | Backend Developer'],
 	    typeSpeed: 20,
