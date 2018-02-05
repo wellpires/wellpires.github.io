8d5bc170f7f0267313b2f04b3a012fb7f293627d
wops <wops@PCALP149.gft.com>
2018-02-05 16:10:48 -0200

    Adicionando a biblioteca 'Typed.js' para o efeito de digitar no título da página principal. Também adicionado a biblioteca de JQuery.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+$('document').ready(function() {
+	var typed = new Typed('#text-info-professional', {
+		strings: ['Wellington Gonçalves Pires' , 'Java World','Backend Developer', 'Wellington Gonçalves Pires | Java World | Backend Developer'],
+	    typeSpeed: 20,
+	    fadeOut: true,
+	    cursorChar: '|'
+	});
+});
